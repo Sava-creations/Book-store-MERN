1276bbdadf755c5a429bf9db87b4c5d4c117d50f
Savandi <savandikodithuwakku@gmail.com>
2025-05-28 23:13:52 +0530

    commits have been changed

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,7 @@
 import express from 'express';
 import {PORT} from './config.js'; 
 
-const app = express(); //define a variable for express app
-// const PORT = process.env.PORT || 3000; //define a variable
-// app.use(express.json()); //middleware to parse JSON
+const app = express();                                                                               //define a variable for express app
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
